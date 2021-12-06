93413c12f86578b2798efdc896261cc1e00952df
Alain Royer <royer_alain@hotmail.com>
2021-12-05 22:05:48 -0500

    day3 part1

---
+++ 2021/day3/day3.py
@@ -0,0 +1,33 @@
+def parse(reports):
+    # bits = [0, 0, 0, 0, 0]
+    bits = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for report in reports:
+        for n in range(0, len(report) - 1):
+            bits[n] += int(report[n])
+
+    gamma = ''
+    epsilon = ''
+
+    for bit in bits:
+        if bit > len(reports) / 2:
+            gamma += '1'
+            epsilon += '0'
+        else:
+            gamma += '0'
+            epsilon += '1'
+
+    return int(gamma, 2), int(epsilon, 2)
+
+
+def main():
+    with open('input1') as f:
+        reports = f.readlines()
+
+    gamma, epsilon = parse(reports)
+
+    consumption = gamma * epsilon
+    print(consumption)
+
+
+if __name__ == '__main__':
+    main()
