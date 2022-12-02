e813a1c38f039fc9d8ee0824224e6601d9b94956
Alain Royer <royer_alain@hotmail.com>
2022-12-01 20:10:14 -0500

    yeah !

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,6 +1,25 @@
-def main():
-    pass
+def main(filename):
+    with open(filename, 'r') as file:
+        items_calories = file.readlines()
+
+        max_colories = []
+        current_total_calories = 0
+
+        for item_calories in items_calories:
+            if item_calories == '\n':
+                max_colories.append(current_total_calories)
+                current_total_calories = 0
+            else:
+                current_total_calories = current_total_calories + \
+                    int(item_calories)
+
+        if current_total_calories != 0:
+            max_colories.append(current_total_calories)
+
+        max_colories.sort(reverse=True)
+
+        print(sum(max_colories[:3]))
 
 
 if __name__ == '__main__':
-    main()
+    main('2022/input1.txt')
