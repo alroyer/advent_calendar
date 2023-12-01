f7ee8074e92654078705bd96027bed074ad71692
Alain Royer <royer_alain@hotmail.com>
2023-12-01 08:53:51 -0500

    day1 part2

--- 2023/day1/part2.py
+++ 2023/day1/part2.py
@@ -1,14 +1,37 @@
+spelled_numbers = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+
 def read_data() -> list[str]:
-    with open('input2.txt', 'r') as f:
+    with open('input1.txt', 'r') as f:
         calibrationValues = f.readlines()
         return calibrationValues
 
 
+def is_spelled_number(line: str) -> tuple[bool, int]:
+    for spelled_number in spelled_numbers.items():
+        if line.startswith(spelled_number[0]):
+            return True, spelled_number[1]
+    return False, 0
+
+
 def compute_calibration_value(line: str) -> int:
     numbers = []
-    for c in line:
+    for index, c in enumerate(line):
+        spelled_number, number = is_spelled_number(line[index:])
         if c.isdigit():
-            numbers.append(c)
+            numbers.append(int(c))
+        elif spelled_number:
+            numbers.append(number)
     return int(f'{numbers[0]}{numbers[-1]}')
 
 
