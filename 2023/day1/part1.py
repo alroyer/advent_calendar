e53d69564a03bd4992b5869c1c69b99d052de2a7
Alain Royer <royer_alain@hotmail.com>
2023-12-01 08:17:10 -0500

    day1 part1

---
+++ 2023/day1/part1.py
@@ -0,0 +1,26 @@
+def read_data() -> list[str]:
+    with open('input1.txt', 'r') as f:
+        calibrationValues = f.readlines()
+        return calibrationValues
+
+
+def compute_calibration_value(line: str) -> int:
+    numbers = []
+    for c in line:
+        if c.isdigit():
+            numbers.append(c)
+    return int(f'{numbers[0]}{numbers[-1]}')
+
+
+def main():
+    lines = read_data()
+
+    sum = 0
+    for line in lines:
+        sum = sum + compute_calibration_value(line)
+
+    print(f'{sum=}')
+
+
+if __name__ == '__main__':
+    main()
