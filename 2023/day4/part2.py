31475f8b24a51e235f6fa1fc6b51b4bb3692685c
Alain Royer <royer_alain@hotmail.com>
2023-12-04 20:07:20 -0500

    day4 part2

--- 2023/day4/part2.py
+++ 2023/day4/part2.py
@@ -9,19 +9,31 @@ def read_data(path: str) -> list[str]:
 
 
 def compute_total(data: list[str]) -> int:
-    gen = {}
+    win = {}
     for card in data:
         card_number, numbers = card.split(':')
         card_number = int(card_number[5:])
         winning_numbers, your_numbers = numbers.split('|')
         winning_numbers = list(filter(None, winning_numbers.split(' ')))
         your_numbers = list(filter(None, your_numbers.split(' ')))
-        count = 0
+        total = 0
         for your_number in your_numbers:
             if your_number in winning_numbers:
-                count += 1
-        gen[card_number] = [card_number + n + 1 for n in range(count)]
-    return 0
+                total += 1
+        win[card_number] = [card_number + n + 1 for n in range(total)]
+
+    total = {}
+    for card_number, copies in win.items():
+        if card_number in total:
+            total[card_number] += 1
+        else:
+            total[card_number] = 1
+        for copy in copies:
+            if copy in total:
+                total[copy] = total[copy] + total[card_number]
+            else:
+                total[copy] = 1
+    return sum(total.values())
 
 
 def main() -> None:
