cb244ca22d7a8dcd77a752e82b9cb5e560bec6e0
Alain Royer <royer_alain@hotmail.com>
2023-12-05 19:35:37 -0500

    part2 :(

--- 2023/day5/part2.py
+++ 2023/day5/part2.py
@@ -3,9 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Range:
-    destination_range_start: int
-    source_range_start: int
-    range_length: int
+    destination_range: tuple[int, int]
+    source_range: tuple[int, int]
 
 
 def read_data(path: str) -> list[str]:
@@ -14,14 +13,16 @@ def read_data(path: str) -> list[str]:
         return [d.strip() for d in data]
 
 
-def parse_data(data: list[str]) -> tuple[list[int], list[tuple[str, list[Range]]]]:
+def parse_data(data: list[str]) -> tuple[list[tuple[int, int]], list[tuple[str, list[Range]]]]:
     seeds = []
     maps = []
     current_map = []
     category = None
     for d in data:
         if d.startswith('seeds'):
-            seeds = [int(x) for x in d[7:].split(' ')]
+            seed_ranges = [int(x) for x in d[7:].split(' ')]
+            for n in range(0, len(seed_ranges), 2):
+                seeds.append((seed_ranges[n], seed_ranges[n] + seed_ranges[n + 1]))
         elif ':' in d:
             if category:
                 maps.append((category, current_map))
@@ -29,43 +30,37 @@ def parse_data(data: list[str]) -> tuple[list[int], list[tuple[str, list[Range]]
             category = d
         elif d:
             destination, source, length = [int(x) for x in d.split(' ')]
-            current_map.append(Range(destination, source, length))
+            current_map.append(Range((destination, destination + length), (source, source + length)))
     if category:
         maps.append((category, current_map))
     return seeds, maps
 
 
-def is_in_range(seed: int, range_: Range) -> int | None:
-    if seed in range(range_.source_range_start, range_.source_range_start + range_.range_length):
-        return range_.destination_range_start + (seed - range_.source_range_start)
-    return None
-
-
-def find_location(source: int, maps: list[tuple[str, list[Range]]]) -> int:
-    location = -1
-    for _, ranges in maps:
-        destination = None
-        for range_ in ranges:
-            destination = is_in_range(source, range_)
-            if destination:
-                location = destination
-                source = destination
-                break
-        if not destination:
-            location = source
-    return location
-
-
-def find_min_location(seeds: list[int], maps: list[tuple[str, list[Range]]]) -> int:
+def find_min_location(seeds: list[tuple[int, int]], maps: list[tuple[str, list[Range]]]) -> int:
     locations = []
-    for n in range(0, len(seeds), 2):
-        for seed in range(seeds[n], seeds[n] + seeds[n + 1]):
-            locations.append(find_location(seed, maps))
-    return min(locations)
+    for _, ranges in maps:
+        for seed in seeds:
+            for range_ in ranges:
+                # TODO
+                start = max(range_.source_range[0], seed[0])
+                end = min(range_.source_range[1], seed[1])
+                if start < range_.source_range[1] and end >= range_.source_range[0]:
+                    locations.append(
+                        (
+                            range_.destination_range[0] + (start - range_.source_range[0]),
+                            range_.destination_range[0] + (end - range_.source_range[0]),
+                        )
+                    )
+                    if seed[0] < range_.source_range[0]:
+                        seeds.append((seed[0], range_.source_range[0]))
+                    if seed[1] >= range_.source_range[1]:
+                        seeds.append((range_.source_range[1], seed[1]))
+                    break
+    return min([l[0] for l in locations])
 
 
 def main() -> None:
-    data = read_data('input1.txt')
+    data = read_data('input0.txt')
     seeds, maps = parse_data(data)
     location = find_min_location(seeds, maps)
     print(f'{location=}')
