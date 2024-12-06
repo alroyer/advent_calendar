62617159ac40305fc0bda9fba4f055b83f9e035f
Alain Royer <royer_alain@hotmail.com>
2024-12-06 13:34:25 -0500

    refactor

--- 2024/day06/part2.py
+++ 2024/day06/part2.py
@@ -2,32 +2,24 @@ from copy import copy
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(frozen=True)
 class Pos:
     x: int
     y: int
     dir: int = 0  # 0=up, 1=right, 2=down, 3=right
 
     def turn_right(self):
-        self.dir += 1
-        self.dir = self.dir % 4
+        return Pos(self.x, self.y, (self.dir + 1) % 4)
 
     def next_move(self):
         if self.dir == 0:
-            return Pos(y=self.y - 1, x=self.x)
+            return Pos(y=self.y - 1, x=self.x, dir=0)
         elif self.dir == 1:
-            return Pos(y=self.y, x=self.x + 1)
+            return Pos(y=self.y, x=self.x + 1, dir=1)
         elif self.dir == 2:
-            return Pos(y=self.y + 1, x=self.x)
+            return Pos(y=self.y + 1, x=self.x, dir=2)
         else:
-            return Pos(y=self.y, x=self.x - 1)
-
-    def move_to(self, pos):
-        self.y = pos.y
-        self.x = pos.x
-
-    def __hash__(self):
-        return hash((self.y, self.x, self.dir))
+            return Pos(y=self.y, x=self.x - 1, dir=3)
 
 
 with open('input1.txt') as f:
@@ -67,8 +59,8 @@ for y in range(h):
             if new_pos.y < 0 or new_pos.y >= h or new_pos.x < 0 or new_pos.x >= w:
                 break
             if new_map[new_pos.y][new_pos.x] == '#':
-                pos.turn_right()
+                pos = pos.turn_right()
             else:
-                pos.move_to(new_pos)
+                pos = new_pos
 
 print(total)
