090d336b27722c3141de3e8a9145c38d3ae80af5
Alain Royer <royer_alain@hotmail.com>
2024-12-09 15:39:04 -0500

    refactor

--- 2024/day09/part2.py
+++ 2024/day09/part2.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 def print_disk(blocks):
     for block in blocks:
-        print('.' * block.size if block.free else f'{block.id}' * block.size, end='')
+        print('.' * block.size if block.free() else f'{block.id}' * block.size, end='')
     print()
 
 
@@ -11,7 +11,7 @@ def next_file(blocks, index=None):
     if index is None:
         index = len(blocks) - 1
     for i in range(index, 0, -1):
-        if not blocks[i].free:
+        if not blocks[i].free():
             return i, blocks[i]
     return -1, None
 
@@ -19,7 +19,7 @@ def next_file(blocks, index=None):
 def next_free(blocks, size, index=None):
     index = index if index is not None else 0
     for i in range(index, len(blocks) - 1):
-        if blocks[i].free and blocks[i].size >= size:
+        if blocks[i].free() and blocks[i].size >= size:
             return i, blocks[i]
     return -1, None
 
@@ -28,7 +28,9 @@ def next_free(blocks, size, index=None):
 class Block:
     id: int
     size: int
-    free: bool = False
+
+    def free(self):
+        return self.id == -1
 
 
 with open('input1.txt') as f:
@@ -43,7 +45,7 @@ for index, size in enumerate(disk_map):
         blocks.append(Block(id, size))
         id += 1
     elif size > 0:
-        blocks.append(Block(-1, size, free=True))
+        blocks.append(Block(-1, size))
 
 file_index, file_block = next_file(blocks)
 while file_block:
@@ -52,11 +54,11 @@ while file_block:
         del blocks[file_index]
         if free_block.size == file_block.size:
             blocks[free_index] = file_block
-            blocks.insert(file_index, Block(-1, file_block.size, free=True))
+            blocks.insert(file_index, Block(-1, file_block.size))
         elif free_block.size > file_block.size:
             blocks[free_index] = file_block
-            blocks.insert(free_index + 1, Block(-1, free_block.size - file_block.size, free=True))
-            blocks.insert(file_index + 1, Block(-1, file_block.size, free=True))
+            blocks.insert(free_index + 1, Block(-1, free_block.size - file_block.size))
+            blocks.insert(file_index + 1, Block(-1, file_block.size))
         file_index, file_block = next_file(blocks, file_index)
     else:
         file_index, file_block = next_file(blocks, file_index - 1)
@@ -66,7 +68,7 @@ total = 0
 index = 0
 for block in blocks:
     for n in range(block.size):
-        if not block.free:
+        if not block.free():
             total += index * block.id
         index += 1
 
