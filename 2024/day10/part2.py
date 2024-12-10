84d62ff42dc1e6fd7fb11e85c2dc1ac8ee8154e4
Alain Royer <royer_alain@hotmail.com>
2024-12-10 12:02:06 -0500

    day10 - part2 🎉

---
+++ 2024/day10/part2.py
@@ -0,0 +1,52 @@
+def print_map(map):
+    for y in range(len(map)):
+        for x in range(len(map[0])):
+            print(map[y][x], end='')
+        print()
+
+
+with open('input5.txt') as f:
+    data = f.read()
+
+map = list(data.split())
+
+# print_map(map)
+
+trailheads = []
+
+height = len(map)
+width = len(map[0])
+
+directions = [
+    (-1, 0),
+    (1, 0),
+    (0, -1),
+    (0, 1),
+]
+
+total_score = 0
+
+for y in range(height):
+    for x in range(width):
+        if map[y][x] == '0':
+            positions = [(y, x, 0)]
+            score = 0
+            while positions:
+                py, px, h = positions.pop()
+                for cy, cx in directions:
+                    yy = py + cy
+                    xx = px + cx
+                    if yy >= 0 and yy < height and xx >= 0 and xx < width:
+                        try:
+                            hh = int(map[yy][xx])
+                            if hh == h + 1:
+                                if hh == 9:
+                                    score += 1
+                                else:
+                                    positions.append((yy, xx, hh))
+
+                        except:
+                            pass
+            total_score += score
+
+print(total_score)
