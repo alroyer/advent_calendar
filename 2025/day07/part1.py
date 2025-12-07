4c2843e625d8869e82af9eeadb3ac662a31ebbf5
Alain Royer <royer_alain@hotmail.com>
2025-12-07 14:27:13 -0500

    day 7 part 1 done

--- 2025/day07/part1.py
+++ 2025/day07/part1.py
@@ -10,30 +10,36 @@ class Point:
         return hash((self.y, self.x))
 
 
-diagram = [
-    ".......S.......",
-    "...............",
-    ".......^.......",
-    "...............",
-    "......^.^......",
-    "...............",
-    ".....^.^.^.....",
-    "...............",
-    "....^.^...^....",
-    "...............",
-    "...^.^...^.^...",
-    "...............",
-    "..^...^.....^..",
-    "...............",
-    ".^.^.^.^.^...^.",
-    "...............",
-]
+# diagram = [
+#     ".......S.......",
+#     "...............",
+#     ".......^.......",
+#     "...............",
+#     "......^.^......",
+#     "...............",
+#     ".....^.^.^.....",
+#     "...............",
+#     "....^.^...^....",
+#     "...............",
+#     "...^.^...^.^...",
+#     "...............",
+#     "..^...^.....^..",
+#     "...............",
+#     ".^.^.^.^.^...^.",
+#     "...............",
+# ]
+
+diagram: list[str] = []
+with open("input1.txt") as f:
+    for line in f.readlines():
+        diagram.append(line.strip())
 
 rows = len(diagram)
 cols = len(diagram[0])
 
 beams: set[Point] = set()
 splitters: set[Point] = set()
+visited: set[Point] = set()
 
 for y in range(rows):
     for x in range(cols):
@@ -42,15 +48,17 @@ for y in range(rows):
         elif diagram[y][x] == "^":
             splitters.add(Point(y, x))
 
-count = 0
 while beams:
     beam = beams.pop()
     while beam not in splitters and beam.y < rows:
+        visited.add(beam)
         beam = Point(beam.y + 1, beam.x)
     if beam in splitters:
-        lpoint = Point(beam.y, beam.x - 1)
-        rpoint = Point(beam.y, beam.x + 1)
-        beams.add(lpoint)
-        beams.add(rpoint)
+        beams.add(Point(beam.y, beam.x - 1))
+        beams.add(Point(beam.y, beam.x + 1))
+
+count = 0
+for splitter in splitters:
+    if Point(splitter.y - 1, splitter.x) in visited:
         count += 1
 print(count)
